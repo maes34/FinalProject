76e4a3528a40aec3d02d3a2f293b8a30874a1432
Maes <iletisim@emrekantarci.com.tr>
2021-02-03 22:41:44 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Businnes.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var x in productManager.GetAll())
+            {
+                Console.WriteLine("Ürün Adı : " + x.ProductName);
+                Console.WriteLine("Ürün Fiyatı : " + x.UnitPrice);
+                Console.WriteLine("Stok : " + x.UnitsInStock);
+                Console.WriteLine("---------------------------");
+            }
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection = true");
+        }
+    }
+}
