048bef0ed68ea7a6ae4f558dbc07ab265168f606
Maes <iletisim@emrekantarci.com.tr>
2021-02-10 22:28:02 +0300

    10 şubat

--- Businnes/Concrete/ProductManager.cs
+++ Businnes/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Businnes.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -17,6 +18,16 @@ namespace Businnes.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult("Ürün ismi min 2 karakter olmalıdır.");
+            }
+            _productDal.Add(product);
+            return new SuccessResult("Ürün eklendi");
+        }
+
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
@@ -25,6 +36,11 @@ namespace Businnes.Concrete
         public List<Product> GetAllByCategory(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(x => x.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
