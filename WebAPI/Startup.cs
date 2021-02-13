8def4987b64a192116bd6f3d2c24fbd698f6c189
Maes <iletisim@emrekantarci.com.tr>
2021-02-13 23:55:14 +0300

    13 şubat

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -34,10 +34,10 @@ namespace WebAPI
             services.AddControllers();
             services.AddSingleton<IProductService,ProductManager>();
             services.AddSingleton<IProductDal,EfProductDal>();
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
-            //});
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,8 +46,8 @@ namespace WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
             app.UseHttpsRedirection();
