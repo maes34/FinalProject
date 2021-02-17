aa6f9a2e659a99bdb84cd9bd8c001e52efdeca76
Maes <iletisim@emrekantarci.com.tr>
2021-02-17 21:44:43 +0300

    Autofac added

---
+++ Businnes/DependencyResolvers/Autofac/AutofacBusinnesModel.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Businnes.Abstract;
+using Businnes.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Businnes.DependencyResolvers.Autofac
+{
+    public class AutofacBusinnesModel : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
